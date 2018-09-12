af3d5850ed93c14dc386049655924ed56855ef51
“zhuhuafan” <zhuhf2@zhongan.com.cn>
2018-09-12 16:51:53 +0800

    454151

---
+++ TabProjectModel/TabProjectModel/FeedBackViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedBackViewController.h
+//  TabProjectModel
+//
+//  Created by ap2 on 2018/9/12.
+//  Copyright © 2018年 ap2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedBackViewController : UIViewController
+
+@end
---
+++ TabProjectModel/TabProjectModel/FeedBackViewController.m
@@ -0,0 +1,40 @@
+//
+//  FeedBackViewController.m
+//  TabProjectModel
+//
+//  Created by ap2 on 2018/9/12.
+//  Copyright © 2018年 ap2. All rights reserved.
+//
+
+#import "FeedBackViewController.h"
+
+@interface FeedBackViewController ()
+
+@end
+
+@implementation FeedBackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)submitaaction:(id)sender {
+    [SVProgressHUD showSuccessWithStatus:@"提交成功"];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
